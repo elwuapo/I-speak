93b3bbe5dacc731f8198de03ecbcf44e9222365a
Nicolas Rivera <nico.riverav@alumnos.duoc.cl>
2018-11-10 15:08:02 -0300

    Makemitions y migrate a ElephantSQL

--- Ispeak/pagweb/models.py
+++ Ispeak/pagweb/models.py
@@ -2,9 +2,8 @@ from django.db import models
 
 # Create your models here.
 
-Class Diccionario():
+class Diccionario(models.Model):
     español = models.CharField(max_length = 50)
     portuges = models.CharField(max_length = 50)
     creolles = models.CharField(max_length = 50)
     ingles = models.CharField(max_length = 50)
-    
