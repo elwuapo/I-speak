f84dcb01094acb5d1a0960d0bcd4a554f9769c29
Nicolas Rivera <nico.riverav@alumnos.duoc.cl>
2018-11-10 15:00:01 -0300

    Agregación de la base de datos y los modelos

--- Ispeak/Ispeak/settings.py
+++ Ispeak/Ispeak/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pagweb',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'Ispeak.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ypszeapv',
+        'USER': 'ypszeapv',
+        'PASSWORD': 'hQJ_BYeGV2jPfdZv39dUuKeHNNtFtiz5',
+        'HOST': 'elmer.db.elephantsql.com',
+        'PORT': '5432',
     }
 }
 
--- Ispeak/pagweb/models.py
+++ Ispeak/pagweb/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+Class Diccionario():
+    español = models.CharField(max_length = 50)
+    portuges = models.CharField(max_length = 50)
+    creolles = models.CharField(max_length = 50)
+    ingles = models.CharField(max_length = 50)
+    
